91adc214d8ff698ca445cbe6db528c705649e0ff
Abe <abealto9@gmail.com>
2022-06-10 16:35:55 -0400

    adding port=3001

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../src/stylesheets/bootstrap.css'
+import '../src/stylesheets/bootstrap.css';
 import { Routes, Route } from 'react-router-dom';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
@@ -8,12 +8,11 @@ import Layout from './pages/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Profile from './pages/Profile';
-import ReviewBoard from'./pages/ReviewBoard';
+import ReviewBoard from './pages/ReviewBoard';
 import SearchResults from './pages/SearchResults';
 
 function App() {
   return (
-    
     <div className='auth-wrapper'>
       <div className='auth-inner'>
         <Routes>
@@ -31,9 +30,6 @@ function App() {
         </Routes>
       </div>
     </div>
-    //     </div>
-
-    // )
   );
 }
 export default App;
