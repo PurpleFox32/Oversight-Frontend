84520704daf7c52f441193472af38b76cb8635c4
Tyler <hippobait28@gmail.com>
2022-06-11 13:57:48 -0500

    Changed UI of the Signup and Login pages some.

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -7,89 +7,89 @@ import Navbar from "../component/navbar";
 const SignUp = () => {
   let navigate = useNavigate();
 
-  const [user, setUser] = useState ({
-    username:"",
-    email:"",
-    password:"",
-    confirmPassword:""
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
   })
-  
+
   function handleChange(e) {
-    setUser({...user,[e.target.name]:e.target.value})
+    setUser({ ...user, [e.target.name]: e.target.value })
   }
 
-  function submitUser(e){
+  function submitUser(e) {
     e.preventDefault();
     API.createUser(user).then(res => {
       console.log(res)
-      navigate("/profile/"+res.data.id)
+      navigate("/profile/" + res.data.id)
     })
   }
-    
 
 
 
-        return (
-          
-            <form onSubmit={submitUser}>
-              <Navbar/>
-            <h3>Sign Up</h3>
-            <div className="mb-3">
-            
-              <label>Username</label>
-              <input
-                onChange={handleChange}
-                name="username"
-                type="text"
-                className="form-control"
-                placeholder="Username"
-              />
-            </div>
-            
-            <div className="mb-3">
-              <label>Email address</label>
-              <input
-                onChange={handleChange}
-                name="email"
-                type="email"
-                className="form-control"
-                placeholder="Enter email"
-              />
-            </div>
-            <div className="mb-3">
-              <label>Password</label>
-              <input
-                onChange={handleChange}
-                name="password"
-                type="password"
-                className="form-control"
-                placeholder="Enter password"
-              />
-              
-            </div>
-            <div className="mb-3">
-              <label>Confirm Password</label>
-              <input
-                onChange={handleChange}
-                name="confirmPassword"
-                type="password"
-                className="form-control"
-                placeholder="Confirm password"
-              />
-              
-            </div>
-            <div className="d-grid">
-              <button type="submit" className="btn btn-primary">
-                Sign Up
-              </button>
-            </div>
-            <p className="forgot-password text-right">
-              Already registered <a href="/sign-in">sign in?</a>
-            </p>
-          </form>
-          
-        );
-        
-      };
-    
-    export default SignUp
+
+  return (
+    <div>
+      <Navbar />
+      <form onSubmit={submitUser} class='container form-control-sm'>
+        <h3>Sign Up</h3>
+        <div className="mb-3">
+
+          <label>Username</label>
+          <input
+            onChange={handleChange}
+            name="username"
+            type="text"
+            className="form-control"
+            placeholder="Username"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            onChange={handleChange}
+            name="email"
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            onChange={handleChange}
+            name="password"
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+
+        </div>
+        <div className="mb-3">
+          <label>Confirm Password</label>
+          <input
+            onChange={handleChange}
+            name="confirmPassword"
+            type="password"
+            className="form-control"
+            placeholder="Confirm password"
+          />
+
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered <a href="/sign-in">sign in?</a>
+        </p>
+      </form>
+    </div>
+  );
+
+};
+
+export default SignUp
