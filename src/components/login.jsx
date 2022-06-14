b179de1c5d6340db3bfb6c1b520376de3c204871
Tyler <hippobait28@gmail.com>
2022-06-14 13:13:34 -0500

    Changed some text and fixed the Navbar issue on login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -7,88 +7,90 @@ import Navbar from "../component/navbar";
 const Login = () => {
   let navigate = useNavigate();
 
-  const [ user, setUser ] = useState({
+  const [user, setUser] = useState({
     username: "",
     password: "",
     id: ""
   })
 
 
-function handleChange(event) {
-  setUser({ ...user, [event.target.name]: event.target.value })
-}
+  function handleChange(event) {
+    setUser({ ...user, [event.target.name]: event.target.value })
+  }
 
 
 
-function submitUser(e) {
-  e.preventDefault();
+  function submitUser(e) {
+    e.preventDefault();
 
-  let parsedId = parseInt(user.id)
+    let parsedId = parseInt(user.id)
 
-  API.getOneById(parsedId).then(res => {
-    console.log(res)
-    navigate("/profile/"+res.data.id)
-  })
-  
-}
+    API.getOneById(parsedId).then(res => {
+      console.log(res)
+      navigate("/profile/" + res.data.id)
+    })
+
+  }
 
 
   return (
-    <form onSubmit={submitUser} class="container">
-      <Navbar/>
-      <h3>Sign In</h3>
-      <div className="mb-3">
-        <label>Username</label>
-        <input
-          onChange={handleChange}
-          name="username"
-          type="text"
-          className="form-control"
-          placeholder="Username"
-        />
-      </div>
-      <div className="mb-3">
-        <label>Password</label>
-        <input
-          onChange={handleChange}
-          name="password"
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-        />
-      </div>
-      <div className="mb-3">
-        <label>Id</label>
-        <input
-          onChange={handleChange}
-          name="id"
-          type="text"
-          className="form-control"
-          placeholder="Enter id"
-        />
-      </div>
-      <div className="mb-3">
-        <div className="custom-control custom-checkbox">
+    <div>
+      <Navbar />
+      <form onSubmit={submitUser} class="container">
+        <h3>Login</h3>
+        <div className="mb-3">
+          <label>Username</label>
           <input
-            type="checkbox"
-            className="custom-control-input"
-            id="customCheck1"
+            onChange={handleChange}
+            name="username"
+            type="text"
+            className="form-control"
+            placeholder="Username"
           />
-          <label className="custom-control-label" htmlFor="customCheck1">
-            Remember me
-          </label>
         </div>
-      </div>
-      <div className="d-grid">
-        <button type="submit" className="btn btn-primary">
-          
-          Submit
-        </button>
-      </div>
-      <p className="forgot-password text-right">
-        Forgot <a href="#">password?</a>
-      </p>
-    </form>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            onChange={handleChange}
+            name="password"
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Id</label>
+          <input
+            onChange={handleChange}
+            name="id"
+            type="text"
+            className="form-control"
+            placeholder="Enter id"
+          />
+        </div>
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+
+            Submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Forgot <a href="#">password?</a>
+        </p>
+      </form>
+    </div>
   );
 };
 
