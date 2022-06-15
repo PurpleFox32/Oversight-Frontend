a59b9f5d9c8381063f7cd8fd6f409d48ccd05c03
Daniel Burgos <dannyb12293@yahoo.com>
2022-06-14 20:50:39 -0400

    About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import NavBar3 from '../components/NavBar3';
-
+import "./home.css";
 
 const About = () => {
   return (
     
    <div><NavBar3/>
-     this is about
+    <div class ='logostyle' >
+     <img src='OverSight-Logo.png' alt='logo' />
+     <h2>About This Project</h2>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis corrupti sit maiores at reiciendis explicabo quibusdam. Tempore a, autem magni, temporibus fugiat perspiciatis alias ipsum accusamus, sequi ducimus fugit cupiditate.
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis corrupti sit maiores at reiciendis explicabo quibusdam. Tempore a, autem magni, temporibus fugiat perspiciatis alias ipsum accusamus, sequi ducimus fugit cupiditate.
+     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum enim fugit consectetur nulla doloribus nisi. Minus temporibus, quis laudantium explicabo maiores, corrupti perspiciatis quas ipsa repudiandae voluptate quidem, nam accusantium?
+     </p>
+     </div>
    </div>
   )
 }
