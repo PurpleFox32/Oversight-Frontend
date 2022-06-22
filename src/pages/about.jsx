b89fd82ec682ccbb19588bcfe576fc3ee67deafd
Daniel Burgos <dannyb12293@yahoo.com>
2022-06-22 09:30:33 -0400

    Changed About paragraph

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,9 +12,16 @@ const about = () => {
       <div class='logostyle' >
         <img src='OverSight-Logo.png' alt='logo' id='logo' />
         <h2>About This Project</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis corrupti sit maiores at reiciendis explicabo quibusdam. Tempore a, autem magni, temporibus fugiat perspiciatis alias ipsum accusamus, sequi ducimus fugit cupiditate.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis corrupti sit maiores at reiciendis explicabo quibusdam. Tempore a, autem magni, temporibus fugiat perspiciatis alias ipsum accusamus, sequi ducimus fugit cupiditate.
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum enim fugit consectetur nulla doloribus nisi. Minus temporibus, quis laudantium explicabo maiores, corrupti perspiciatis quas ipsa repudiandae voluptate quidem, nam accusantium?
+        <p>
+          
+        This application is a video game review platform. Users will be able to review their favorite video game(s) or 
+critique a game by writing a post and engaging other users to discuss the video game.<br/>
+<b>Project Background:</b> <br/>
+We are making this so game developers can hear from their player base about various issues and concerns. This 
+can include bug reports, reviews of the game, what can be improved, ideas to add to the game, and things that 
+players like. Players can also find forums to the games they are playing easily through this application and details 
+about the game. Users can find groups and other players to interact with and play games.
+
         </p>
       </div>
     </div>
