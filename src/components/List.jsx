11fab2cec6786e370aadff91741e3754bd556baf
Corey Olson <coreyolson616@gmail.com>
2022-06-22 14:43:52 -1000

    list component and other things

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import { React, useState } from 'react';
+import data from '../games.json'
+import '../stylesheets/List.css'
 
-const List = () => {
+
+function List(props) {
+ 
   return (
-    <div>
-      <h2>Tst</h2>
-    </div>
+    <div className="List">
+      <div className="games">
+      <ul>
+          {data.map((game) => (
+            <div key={ game.id } className="games">
+              <h4>Image will go here</h4>
+              <h2>{game.title} Game Title</h2>
+              <p>{game.description} Game Details</p>
+              </div>
+          ))}
+      </ul>
+      </div>
+      </div>
   )
 }
 
 export default List
+
+
+
 
