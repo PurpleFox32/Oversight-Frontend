628326be4b8000681341a695d58b59e46882b871
Abe <abealto9@gmail.com>
2022-06-27 18:18:52 -0400

    added a route to the DisplayGame.jsx file to display post

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,9 +2,10 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import '../stylesheets/List.css';
 import NavBar from './NavBar2';
+import { Link } from 'react-router-dom';
 
 function List(props) {
-  const [games, setGames] = useState({});
+  const [games, setGames] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:3001/games/list').then((response) => {
@@ -13,20 +14,25 @@ function List(props) {
     });
   }, []);
 
+  function getGames() {
+    return games.map((game) => (
+      <div key={game.GameId} id='listItem'>
+        <img src={game.GameThumbnail} />
+        <h1>{game.Name}</h1>
+        <h3>{game.Description}</h3>
+        <Link to={`/oneGame/${game.GameId}`}>view</Link>
+      </div>
+    ));
+  }
+
   return (
     <div>
       <NavBar />
 
       <div className='row'>
-        {/* <div>
-          {games.map((game) => (
-            <div key={game.GameId} id='listItem'>
-              <img src={game.GameThumbnail} />
-              <h1>{game.Name}</h1>
-              <h3>{game.Description}</h3>
-            </div>
-          ))}
-        </div> */}
+        <div className='gameList'>
+          {games === null ? <div>loading</div> : getGames()}
+        </div>
       </div>
     </div>
   );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import API from '../UTILS/API';
-import "../stylesheets/Rating.css";
+import '../stylesheets/Rating.css';
 //import "../games.json"
 
-
 const Rating = () => {
-  const [body, setBody] = useState("");
+  const [body, setBody] = useState('');
   const [user, setUser] = useState({});
   const { id } = useParams();
-
-
 
   //console.log(setBody.data);
 
@@ -21,89 +18,86 @@ const Rating = () => {
     //   console.log(user)
     // })
 
-    let userToken = JSON.parse(localStorage.getItem("userToken"));
+    let userToken = JSON.parse(localStorage.getItem('userToken'));
 
     function fetch() {
-      console.log(userToken)
-      API.getUserData(userToken).then(response => {
-        console.log(response.data)
-        setUser(response.data)
-      })
-
-      
-
-
+      console.log(userToken);
+      API.getUserData(userToken).then((response) => {
+        console.log(response.data);
+        setUser(response.data);
+      });
     }
 
+    function getGame() {
+      API.getPost(12).then((res) => {
+        console.log(res);
+      });
+    }
+
+    getGame();
+
     fetch();
-  }, []
-  
-  )
- 
+  }, []);
 
   function submitReview(e) {
     e.preventDefault();
     console.log(body);
-    let userToken = JSON.parse(localStorage.getItem("userToken"));
+    let userToken = JSON.parse(localStorage.getItem('userToken'));
 
-    API.createPost({body}, userToken).then(res => {
-      console.log(res)
-    }).then(()=>{
-      alert("succeful")
-    })
+    API.createPost({ body }, userToken)
+      .then((res) => {
+        console.log(res);
+      })
+      .then(() => {
+        alert('succeful');
+      });
   }
 
   return (
     <div>
-    <div class="game-info">
-      <div>
-      <div class="card mb-3">
-  <div class="row g-6">
-    <div class="col-md-4">
-      <img
-        src="https://imgs.callofduty.com/content/dam/atvi/callofduty/cod-touchui/kronos/common/social-share/social-share-image.jpg"
-        alt=""
-        class="img-fluid rounded"
-      />
-    </div>
-    <div class="col-md-5">
-      <div class="card-body">
-        <h5 class="card-title"></h5>
-        <p class="card-text">
-        Call of Duty is a first-person shooter video game franchise published by Activision. Starting out in 2003, it first focused on games set in World War II. Over time, 
-        the series has seen games set in the midst of the Cold War, futuristic worlds, and outer space. 
-        </p>
+      <div class='game-info'>
+        <div>
+          <div class='card mb-3'>
+            <div class='row g-6'>
+              <div class='col-md-4'>
+                <img
+                  src='https://imgs.callofduty.com/content/dam/atvi/callofduty/cod-touchui/kronos/common/social-share/social-share-image.jpg'
+                  alt=''
+                  class='img-fluid rounded'
+                />
+              </div>
+              <div class='col-md-5'>
+                <div class='card-body'>
+                  <h5 class='card-title'></h5>
+                  <p class='card-text'>
+                    Call of Duty is a first-person shooter video game franchise
+                    published by Activision. Starting out in 2003, it first
+                    focused on games set in World War II. Over time, the series
+                    has seen games set in the midst of the Cold War, futuristic
+                    worlds, and outer space.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
-    </div>
-  </div>
-  </div>
-</div>
-    
-
-
-  </div>
-
-    <div className="create">
-      <form>
-        <label>Review</label>
-        <textarea
-          required
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-        ></textarea>
-        <button onClick={submitReview}>Add Review</button>
-        
-
-        
-      </form>
-      <div>
-          <h1>{user.username}</h1> 
+      <div className='create'>
+        <form>
+          <label>Review</label>
+          <textarea
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+          <button onClick={submitReview}>Add Review</button>
+        </form>
+        <div>
+          <h1>{user.username}</h1>
+        </div>
       </div>
     </div>
-    </div>
-    
-   
   );
 };
 
