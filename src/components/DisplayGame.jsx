96b521f60fb3f983fc429a9414b637fc3b123a69
Tyler Babcock <purplefox32.minecraft@gmail.com>
2022-07-01 12:08:35 -0500

    Fixed Post Rendering

--- src/components/DisplayGame.jsx
+++ src/components/DisplayGame.jsx
@@ -3,7 +3,6 @@ import { useParams } from 'react-router-dom';
 import NavBar2 from './NavBar2';
 import API from '../UTILS/API';
 import '../stylesheets/bootstrap.css'
-import axios from 'axios';
 
 const DisplayGame = () => {
   const params = useParams();
@@ -13,37 +12,24 @@ const DisplayGame = () => {
 
   useEffect(() => {
 
-    let url = 'http://localhost:3001/games/' + params.id;
-
     //Get the single game
     function getGame() {
-      API.getGame(params.gameId).then((res) => {
-        console.log(res.data);
-        setGame(res.data);
+      API.getGame(params.gameId).then((response) => {
+        console.log(response.data);
+        setGame(response.data);
       });
     }
 
-    /********************** Get posts *****************************/
-
-    // function getPosts() {
-    //   API.getPosts(params.gameId).then((res) => {
-    //     console.log(res.data);
-    //     setPost(res.data);
-    //   })
-    // }
-
-    //get game that was selected and display all post.
-    //user should then be able to add a post(review)
-
-    axios.get(url).then((response) => {
-      console.log(response);
-      setPost(response.data)
-    })
-
-    /**************************************************************/
+    //Get Posts by GameId
+    function getPosts() {
+      API.getPosts(params.gameId).then((res) => {
+        console.log(res.data.Posts);
+        setPost(res.data.Posts);
+      })
+    }
 
     getGame();
-    // getPosts([]);
+    getPosts([]);
   }, [params.gameId, params.id]);
 
   const handleChange = (e) => {
@@ -58,12 +44,17 @@ const DisplayGame = () => {
     });
   };
 
-  function getPosts() {
-    return posts.map((post) => (
-      <div key={post.gameId} id='list'>
-        <p>{post.post}</p>
-      </div>
-    ))
+  // function getPosts() {
+  //   return posts.map((post) => (
+  //     <div key={post.gameId} id='list'>
+  //       <p>{post.post}</p>
+  //     </div>
+  //   ))
+  // }
+
+  //Reload Page
+  function refreshPage() {
+    window.location.reload(true);
   }
 
   return (
@@ -72,11 +63,15 @@ const DisplayGame = () => {
       <img src={game.GameThumbnail} alt='thumbnail' />
       <h1>{game.Name}</h1>
       <div className='postList flex-wrap'>
-        {getPosts()}
+        {posts.map((post) => (
+      <div key={post.gameId} id='list'>
+        <p>{post.post}</p>
+      </div>
+    ))}
       </div>
       <form onSubmit={handleSubmit}>
         <input onChange={handleChange} placeholder='type a review' />
-        <button type='submit'>Submit</button>
+        <button type='submit' onClick={refreshPage} >Submit</button>
       </form>
     </div>
   );
