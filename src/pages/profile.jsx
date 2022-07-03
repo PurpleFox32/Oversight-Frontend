a8db3d7c0557a9f0016e1da9d28ad6cf78bbd166
Abe <abealto9@gmail.com>
2022-07-03 12:20:34 -0400

    fixed/added getUserData fetch to profile.jsx

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import NavBar from '../components/Profile-Nav';
+import API from '../UTILS/API';
 import axios from 'axios';
 
 const Profile = () => {
@@ -8,28 +9,41 @@ const Profile = () => {
   const [user, setUser] = useState([]);
 
   useEffect(() => {
-    let url = 'http://localhost:3001/users/profile';
+    // let url = 'http://localhost:3001/users/profile';
 
-    axios.get(url).then((response) => {
-      console.log(response);
-      setUser(response.data);
-    })
+    // axios.get(url).then((response) => {
+    //   console.log(response);
+    //   setUser(response.data);
+    // })
+    let userToken = JSON.parse(localStorage.getItem('userToken'));
 
-  }, [params.id]
-  )
+    function fetch() {
+      console.log(userToken);
+      API.getUserData(userToken).then((response) => {
+        console.log(response);
+        setUser(response.data);
+      });
+    }
+    fetch();
+    // params.id
+  }, []);
 
   return (
     <div className='view-user'>
       <NavBar />
       <h1>Welcome {user.username} </h1>
-      <hr style={{
+      <hr
+        style={{
           color: '#32FBE2',
-          height: '4px'
-        }}/>
+          height: '4px',
+        }}
+      />
       <h3>Email: {user.email}</h3>
-      <h6>User Id: {user.user_id} || Date Created: {user.createdAt}</h6>
+      <h6>
+        User Id: {user.user_id} || Date Created: {user.createdAt}
+      </h6>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
